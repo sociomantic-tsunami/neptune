9b8897b782919cf82549eb7086f41ec274b6f1ea
Mathias L. Baumann <mathias.baumann@sociomantic.com>
2017-05-23 19:38:54 +0200

    First version of the neptune tool
    Supports only patch-releases, does not support any conflict resolvings or other
    fancy things.
    
    Auto-detects what kind of release to do based on the currently checked out
    branch.
    
    Will always check with the user before doing any modifications.

---
+++ src/neptune/api.d
@@ -0,0 +1,166 @@
+/*******************************************************************************
+
+    helper methods to access the octod/github API
+
+    Copyright:
+        Copyright (c) 2017 Sociomantic Labs GmbH. All rights reserved.
+
+    License:
+        Boost Software License Version 1.0. See LICENSE.txt for details.
+
+*******************************************************************************/
+
+module neptune.api;
+
+import octod.core;
+import octod.api.repos;
+
+
+/*******************************************************************************
+
+    Returns:
+        global api object to interact with github
+
+*******************************************************************************/
+
+public ref HTTPConnection api ( )
+{
+
+    static HTTPConnection _api;
+    static bool connected = false;
+
+    if (!connected)
+    {
+        _api = HTTPConnection.connect(getConf());
+        connected = true;
+    }
+
+    return _api;
+}
+
+
+/*******************************************************************************
+
+    Returns:
+        neptune configuration. If it doesn't exists, will prompt the user for
+        the required infomation
+
+*******************************************************************************/
+
+public Configuration getConf ( )
+{
+    import neptune.cmd;
+
+    Configuration cfg;
+    cfg.dryRun = false;
+
+    try cfg.oauthToken = cmd("git config neptune.oauthtoken");
+    catch (ExitCodeException) {}
+
+    if (cfg.oauthToken.length == 0)
+        return githubSetup();
+
+    return cfg;
+}
+
+
+/*******************************************************************************
+
+    Sets up the interaction with github using an oauth token
+
+    Returns:
+        set up configuration object
+
+*******************************************************************************/
+
+private Configuration githubSetup ( )
+{
+    import neptune.cmd;
+
+    import std.stdio;
+    import std.string;
+
+    string hub_oauth;
+
+    Configuration cfg;
+    cfg.dryRun = false;
+
+    try hub_oauth = cmd("git config hub.oauthtoken");
+    catch (ExitCodeException) {}
+
+    if (hub_oauth.length > 0)
+    {
+        writefln("No oauth token was found under neptune.oauthtoken, however an hub.oauthtoken was found. ");
+        if (readYesNoResponse("Would you like to use it as neptune.oauthtoken config?"))
+        {
+            cmd("git config --global neptune.oauthtoken " ~ hub_oauth);
+            cfg.oauthToken = hub_oauth;
+            return cfg;
+        }
+    }
+
+    writefln("Setting up an oauth token for github access");
+    writef("Please enter your username: ");
+
+    cfg.username = strip(readln());
+
+    while (cfg.username.length == 0)
+    {
+        writef("Empty input. Please provide your username: ");
+        cfg.username = strip(readln());
+    }
+
+    writef("Please provide your password: ");
+    {
+        import core.sys.posix.termios;
+
+        // Disable terminal echo for password entering
+        termios old, _new;
+        if (tcgetattr(stdout.fileno, &old) != 0)
+            throw new Exception("Unable to fetch termios attr");
+
+        _new = old;
+        _new.c_lflag &= ~ECHO;
+
+        if (tcsetattr (stdout.fileno, TCSAFLUSH, &_new) != 0)
+            throw new Exception("Unable to set termios attr");
+
+        // Reenable upon scope exit
+        scope(exit)
+            tcsetattr(stdout.fileno, TCSAFLUSH, &old);
+
+        cfg.password = readln()[0 .. $-1];
+
+        while (cfg.password.length == 0)
+        {
+            writef("Empty input. Please provide your password: ");
+            cfg.password = strip(readln());
+        }
+    }
+
+    import octod.core;
+    import vibe.data.json;
+
+    //import vibe.core.log;
+    //setLogLevel(LogLevel.trace);
+
+    auto client = HTTPConnection.connect(cfg);
+
+    Json json = Json.emptyObject;
+
+    json["scopes"] = Json.emptyArray;
+    json["scopes"] ~= "repo";
+    json["note"] = "Neptune";
+
+    auto response = client.post("/authorizations", json);
+
+    cfg = cfg.init;
+    cfg.dryRun = false;
+    cfg.oauthToken = response["token"].to!string;
+
+    import neptune.cmd;
+
+    cmd("git config --global neptune.oauthtoken " ~ cfg.oauthToken);
+
+    return cfg;
+}
---
+++ src/neptune/main.d
@@ -0,0 +1,278 @@
+/*******************************************************************************
+
+    Library release tool.
+
+    Copyright:
+        Copyright (c) 2017 Sociomantic Labs GmbH. All rights reserved.
+
+    License:
+        Boost Software License Version 1.0. See LICENSE.txt for details.
+
+*******************************************************************************/
+
+module neptune.main;
+
+import neptune.api;
+import neptune.versionHelper;
+import neptune.mergeHelper;
+import neptune.cmd;
+
+import octod.api.repos;
+
+/*******************************************************************************
+
+    Programs entry point
+
+*******************************************************************************/
+
+void main ( )
+{
+    import std.stdio;
+    import std.algorithm : map, sort;
+    import std.range : array;
+
+    //import vibe.core.log;
+    //setLogLevel(LogLevel.trace);
+
+    auto repo = api.repository(getUpstream());
+    auto tags = repo.releasedTags().map!(a=>Version(a.name)).array;
+
+    sort(tags);
+
+    cmd("git remote update");
+
+    auto release = autodetect(tags);
+
+    ActionList list;
+
+    if (release.type == release.type.Patch)
+        list = preparePatchRelease(repo, tags, release);
+    else
+    {
+        writefln("This seems to be %s release which is not yet supported, exiting...",
+                 release.type);
+        return;
+    }
+
+    writefln("Actions to be done:");
+
+    foreach (action; list.local_actions)
+        writefln(" * %s (%s)", action.description, action.command);
+
+    if (!readYesNoResponse("\nWould you like to continue?"))
+    {
+        writefln("Aborting...");
+        return;
+    }
+
+
+    writefln("");
+    foreach (action; list.local_actions)
+    {
+        import std.string : strip;
+
+        writefln("%s ...\n\t%s",
+                 action.description,
+                 action.command);
+        writefln("\t%s", strip(cmd(action.command)));
+    }
+
+    writefln("Done! Some references should be pushed: %s", list.affected_refs);
+
+    if (!readYesNoResponse("Would you like to push the modified references now?"))
+    {
+        writefln("Aborting...");
+        return;
+    }
+
+    import std.format;
+    cmd(format("git push %s %(%s %)", getRemote(), list.affected_refs));
+}
+
+
+/***************************************************************************
+
+    Creates local versions of all given remote branches
+
+    Params:
+        remote branches to create local branches from
+
+***************************************************************************/
+
+void createLocalBranches ( R ) ( R branches )
+{
+    import std.format;
+
+    auto upstream = getRemote();
+
+    foreach (branch; branches)
+        cmd(format("git branch -f %s %s/%s", branch, upstream, branch));
+}
+
+
+/*******************************************************************************
+
+    Prepares the actions for a patch release
+
+    Params:
+        repo = repo object
+        tags = array of tags for this repo
+        patch_version = version to release
+
+    Returns:
+        prepared ActionList object with all refs/actions required for this
+        release
+
+*******************************************************************************/
+
+ActionList preparePatchRelease ( ref Repository repo, Version[] tags,
+                                 Version patch_version )
+{
+    import neptune.github;
+    import neptune.mergeHelper;
+
+    import std.format;
+    import std.algorithm;
+    import std.range;
+    import std.stdio;
+
+    bool thisVersion ( Version v )
+    {
+        return v.major == patch_version.major &&
+               v.minor == patch_version.minor;
+    }
+
+    bool newerVersion ( Version v )
+    {
+        return v > patch_version;
+    }
+
+    bool ourMajorRelease ( Version v )
+    {
+        return v.major == patch_version.major &&
+               v.minor.isNull && v.patch.isNull;
+    }
+
+    // Get version tracking branches
+    auto branches = getBranches(repo)
+                      .map!(a=>Version(a.name))
+                      .filter!(a=>!thisVersion(a) &&
+                                  (newerVersion(a) ||
+                                  ourMajorRelease(a)))
+                      .array();
+
+    branches.sort();
+
+    createLocalBranches(branches);
+
+    auto current_branch = getCurrentBranch();
+
+    ActionList list;
+
+    scope(exit)
+        list.local_actions ~= LocalAction(format("git checkout %s", current_branch),
+                                          format("Checkout original branch %s",
+                                             current_branch));
+
+    scope releaser = new PatchMerger(branches, tags);
+
+    list ~= releaser.release(Version(current_branch));
+
+    return list;
+}
+
+
+/*******************************************************************************
+
+    Params:
+        release_version = release version to make
+        target = target reference for the release
+
+    Returns:
+        an actionlist element containing all the actions/refs required to do the
+        requested release
+
+*******************************************************************************/
+
+ActionList makeRelease ( Version release_version, string target )
+{
+    assert(!release_version.major.isNull &&
+           !release_version.minor.isNull &&
+           !release_version.patch.isNull);
+
+    import std.format;
+    auto v = release_version.toString();
+
+    return ActionList([LocalAction(format("git tag -m %s %s %s", v, v, target),
+                                  format("Create annotated tag %s", v))],
+                      [v],
+                      [v]);
+}
+
+
+/*******************************************************************************
+
+    Autodetects what kind of release and which version most likely needs to be
+    done
+
+    The type of the release is detected according to this table:
+
+    MA = current branch major version number
+    MI = current branch minor version number
+    eqMA = equal major version tag existing
+    eqMI = equal minor version tag existing
+
+    MA MI eqMA eqMI type    // example
+    0  1  1    0    Minor   // tag 2.0.0  branch 2.1.x
+    1  0  1    0    Minor   // tag 3.1.2  branch 3.x.x
+    0  1  1    1    Patch   // tag 2.1.0  branch 2.1.x
+    1  0  0    0    Major   // tag 2.0.0  branch 3.x.x
+
+    Params:
+        tags = tags of the repository
+
+    Returns:
+        most likely version to be released
+
+*******************************************************************************/
+
+Version autodetect ( Version[] tags )
+{
+    import neptune.cmd;
+    import neptune.versionHelper;
+
+    import octod.api.repos;
+
+    import std.stdio;
+    import std.range;
+    import std.algorithm;
+
+    auto current = Version(getCurrentBranch());
+
+    if (current.major.isNull)
+    {
+        throw new Exception("This does not seem to be a version branch!");
+    }
+
+    assert(tags.length > 0, "No tags found?!");
+
+    auto matching_major = tags.retro.find!(a=>a.major == current.major);
+    auto matching_minor = tags.retro.find!(a=>a.major == current.major &&
+                                              a.minor == current.minor);
+
+    auto release_version = needPatchRelease(matching_major,
+                                            matching_minor, current);
+
+    if (release_version.type == Version.Type.Invalid)
+        release_version = needMinorRelease(matching_major,
+                                           matching_minor, current);
+
+    if (release_version.type == Version.Type.Invalid)
+        release_version = needMajorRelease(matching_major,
+                                           matching_minor, current);
+
+    writefln("We are on branch %s", current);
+    writefln("Detected release %s", release_version);
+
+    return release_version;
+}

527af2a59e84440c4348583228cf348fa45caf69
Mathias L. Baumann <mathias.baumann@sociomantic.com>
2018-02-26 19:16:00 +0100

    Add testsuite for patch releases

---
+++ integrationtest/patch_release/main.d
@@ -0,0 +1,126 @@
+/*******************************************************************************
+
+    Tests the 'first release' scenario with the neptune-release tool
+
+    Copyright:
+        Copyright (c) 2017 Sociomantic Labs GmbH. All rights reserved.
+
+    License:
+        Boost Software License Version 1.0. See LICENSE.txt for details.
+
+*******************************************************************************/
+
+module integrationtest.patch_release.main;
+
+import integrationtest.common.GHTestServer;
+
+import integrationtest.common.TestCase;
+
+import integrationtest.common.shellHelper;
+
+class PatchRelease : TestCase
+{
+    this ( )
+    {
+        super(8004);
+    }
+
+    override protected void run ( )
+    {
+        import std.stdio: toFile;
+
+        // Create a v1.0.0 dummy
+        git.cmd("git checkout -B v1.x.x");
+        toFile("bla", git ~ "/somefile.txt");
+        git.cmd("git add somefile.txt");
+        git.cmd(["git", "commit", "-m", "Add some file"]);
+        git.cmd(`git tag -a v1.0.0 -m v1.0.0`);
+
+        auto sha1 = git.cmd("git rev-parse v1.0.0");
+
+        with (this.fake_github)
+        {
+            import std.typecons;
+            import std.range;
+
+            // Also create the release in the fake-github server
+            releases ~= Release("v1.0.0", "v1.0.0", "", sha1);
+            tags ~=     Ref("v1.0.0", sha1);
+            branches ~= Ref("v1.x.x", sha1);
+            branches ~= Ref("v1.0.x", sha1);
+
+            milestones ~= Milestone(
+               10, // id
+               20, // number
+               "v1.0.1", // title
+               "https://github.com/sociomantic/sandbox/milestone/20", // html url
+               "open", // state
+               0, // open issues
+               3); // closed issues
+
+            issues = [
+                Issue(
+                    "Terrible bug", // title
+                    55, // number
+                    "closed", // state
+                    nullable(milestones.front)),
+                Issue(
+                    "Sneaky bug", // title
+                    56, // number
+                    "closed", // state
+                    nullable(milestones.front)),
+                Issue(
+                    "Obvious bug", // title
+                    57, // number
+                    "closed", // state
+                    nullable(milestones.front)),
+                Issue(
+                    "Unrelated bug", // title
+                    58, // number
+                    "open", // state
+                    Nullable!Milestone()),
+                Issue(
+                    "Completely unrelated bug", // title
+                    59, // number
+                    "closed", // state
+                    Nullable!Milestone())
+                    ];
+        }
+
+        git.cmd("git checkout -B v1.0.x");
+
+
+        auto neptune = this.startNeptuneRelease();
+
+        // Capture stdout/stderr
+        string stdout = getAsyncStream(neptune.stdout);
+        string stderr = getAsyncStream(neptune.stderr);
+
+        scope(failure)
+        {
+            import std.stdio;
+
+            writefln("Failure! Neptune output was:\n------\n%s\n-----\n%s",
+                stderr, stdout);
+        }
+
+        this.checkTerminationStatus();
+        this.checkReleaseMail(stdout);
+
+        assert(this.fake_github.milestones[0].state == "closed");
+    }
+}
+
+/*******************************************************************************
+
+    Main function, sets up tests & runs event loop
+
+*******************************************************************************/
+
+version(UnitTest) {} else
+void main ( )
+{
+    auto test = new PatchRelease();
+
+    test.startTest();
+}

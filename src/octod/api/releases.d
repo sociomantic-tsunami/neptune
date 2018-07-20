f1113312181102fa0b5196b87ac9ad2559fe22df
Mihails Strasuns <mihails.strasuns@gmail.com>
2018-07-20 17:48:31 +0300

    Move octod source to the neptune repo
    Because of plans to both move to GitLab and transition to using GrapQL
    API where applicable there is a little value in octod as a separate
    project anymore and it is moved in-place as legacy dependency.

---
+++ src/octod/api/releases.d
@@ -0,0 +1,53 @@
+/*******************************************************************************
+
+    Provides wrappers on top of some methods documented in
+    https://developer.github.com/v3/repos/releases/
+
+    Copyright: Copyright (c) 2016 Sociomantic Labs GmbH. All rights reserved
+
+    License: Boost Software License Version 1.0 (see LICENSE for details)
+
+*******************************************************************************/
+
+module octod.api.releases;
+
+import octod.api.repos;
+import octod.core;
+
+import std.typecons;
+
+/*******************************************************************************
+
+    Creates a new release on github
+
+    Params:
+        connection = connection to use
+        repo = repository object
+        tag  = tag used for the release
+        title = title for the release
+        content = content of the release
+        prerelease = true if this is a prerelease, else false
+
+*******************************************************************************/
+
+public void createRelease ( ref HTTPConnection connection, ref Repository repo,
+    string tag, string title, string content,
+    Flag!"prerelease" prerelease = No.prerelease )
+{
+    import std.format;
+    import vibe.data.json;
+
+    auto owner = repo.json["owner"]["login"].get!string();
+    auto name = repo.name();
+
+    Json json = Json.emptyObject;
+    json["tag_name"] = tag;
+    json["name"] = title;
+    json["body"] = content;
+    json["target_committish"] = tag;
+    json["prerelease"] = prerelease == Yes.prerelease;
+
+    auto url = format("/repos/%s/%s/releases", owner, name);
+
+    connection.post(url, json);
+}

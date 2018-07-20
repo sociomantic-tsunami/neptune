64923f61ae8810e4782aea12e02792fe9cff9bda
Mihails Strasuns <mihails.strasuns@gmail.com>
2018-07-20 17:48:31 +0300

    Move octod source to the neptune repo
    Because of plans to both move to GitLab and transition to using GrapQL
    API where applicable there is a little value in octod as a separate
    project anymore and it is moved in-place as legacy dependency.

---
+++ src/octod/api/repos.d
@@ -0,0 +1,419 @@
+/*******************************************************************************
+
+    Provides wrappers on top of some methods documented in
+    https://developer.github.com/v3/repos/
+
+    Copyright: Copyright (c) 2016 Sociomantic Labs GmbH. All rights reserved
+
+    License: Boost Software License Version 1.0 (see LICENSE for details)
+
+*******************************************************************************/
+
+module octod.api.repos;
+
+import vibe.data.json;
+import octod.core;
+import octod.media;
+import octod.api.common;
+
+/**
+    Aggregate for git reference description
+
+    There are quite some places in GitHub JSON responses where references are
+    referred either by name or by sha exclusively. This API tries to provide
+    both when feasible and wraps it with this struct.
+ **/
+struct GitRef
+{
+    string name;
+    string sha;
+}
+
+/// Alias for old name of the struct
+deprecated alias Tag = GitRef;
+
+/**
+    Wraps connection and repository metadata for simple shortcut access
+    to repository related API methods. Arbitrary fields can be accessed
+    via `json` getter.
+ **/
+struct Repository
+{
+    mixin CommonEntityMethods;
+
+    /**
+        Returns:
+            repository name
+     **/
+    string name ( )
+    {
+        return this.json["name"].get!string();
+    }
+
+    /**
+        Returns:
+            programming language used for majority of repository files
+     **/
+    string language ( )
+    {
+        return this.json["language"].get!string();
+    }
+
+    /**
+        Makes an API request to resolve specified git reference name to
+        its SHA hash in this repo.
+
+        Params:
+            refname = git ref (like tag or branch) name
+
+        Returns:
+            SHA of commit matching the reference
+     **/
+    string resolveGitReference ( string refname )
+    {
+        import std.format;
+
+        auto owner = this.json["owner"]["login"].get!string();
+        auto name = this.name();
+        auto url = format("/repos/%s/%s/commits/%s", owner, name, refname);
+        auto json = this.connection.get(url, MediaType("", "sha"));
+        return json.get!string();
+    }
+
+    /**
+        Fetches all repository branches
+
+        Returns:
+            array of gitref structs for all GitHub branches in this repo
+     **/
+    GitRef[] branches ( )
+    {
+        import std.format;
+        import std.algorithm;
+        import std.range;
+
+        import vibe.data.json;
+
+        auto owner = this.json["owner"]["login"].get!string();
+        auto name = this.name();
+
+        auto url = format("/repos/%s/%s/branches", owner, name);
+        auto json_branches = this.connection.get(url).get!(Json[]);
+
+        GitRef toRef ( Json branch )
+        {
+           return GitRef(branch["name"].get!string,
+                       branch["commit"]["sha"].to!string);
+        }
+
+        return json_branches.map!toRef.array;
+    }
+
+    /**
+        Fetches repository tags filtered to only released ones
+
+        This utility is useful for projects that strictly require all public
+        releases to be actual GitHub releases, making possible to ignore any
+        other tags that may exist in the project.
+
+        There is a GitHub API to get both releases and tags, but former lacks
+        SHA information and latter has no information about releases. This
+        method makes request to both and merges information into one entity.
+
+        Returns:
+            array of gitref structs for all GitHub releases in this repo
+     **/
+    GitRef[] releasedTags ( )
+    {
+        import std.format;
+        import std.array;
+        import std.algorithm.iteration : map, filter;
+        import std.algorithm.searching : find;
+
+        auto owner = this.json["owner"]["login"].get!string();
+        auto name = this.name();
+
+        auto url = format("/repos/%s/%s/releases", owner, name);
+        auto json_releases = this.connection.get(url).get!(Json[]);
+
+        url = format("/repos/%s/%s/tags", owner, name);
+        auto json_tags = this.connection.get(url).get!(Json[]);
+
+        GitRef resolveTag ( Json release )
+        {
+            auto tag_name = release["tag_name"].get!string();
+            auto tag = json_tags
+                .find!(json => json["name"].get!string() == tag_name);
+            if (tag.empty)
+                return GitRef.init;
+            else
+                return GitRef(tag_name, tag.front["commit"]["sha"].to!string());
+        }
+
+        return json_releases
+            .filter!(a => !a["draft"].get!bool)
+            .map!resolveTag
+            // refs that failed to resolve will have empty name:
+            .filter!(gitref => gitref.name.length)
+            .array();
+    }
+
+    /**
+        Provides access to repository content
+
+        Params:
+            path = relative path in the repository to request
+            gitref = branch/tag to use. If empty, default branch will be used.
+
+        Returns:
+            information about found entity (file/directory/submodule/symlink)
+            stored in a wrapper struct
+     **/
+    RepositoryEntity download ( string path, string gitref = "" )
+    {
+        import std.format;
+
+        auto url = format(
+            "/repos/%s/%s/contents/%s",
+            this.json["owner"]["login"].get!string(),
+            this.name(),
+            path
+        );
+
+        if (gitref.length)
+            url ~= "?ref=" ~ gitref;
+
+        return RepositoryEntity(this.connection.get(url));
+    }
+}
+
+/**
+    Struct representing some entity stored in a git repository
+
+    Exact kind of entity can be checked by calling `RepositoryEntity.kind`, and
+    more strongly typed wrapper structs can be retrieved by
+    `RepositoryEntity.expectXXX` methods.
+ **/
+struct RepositoryEntity
+{
+    /**
+        Represents a file stored in the repository
+     **/
+    static struct File
+    {
+        const Json json;
+
+        private this ( Json json )
+        {
+            this.json = json;
+        }
+
+        /**
+            Returns:
+                Decoded content of the file (if < 1Mb)
+         **/
+        immutable(void)[] content ( )
+        {
+            import std.exception : enforce;
+            import std.base64;
+            import std.range : join;
+            import std.algorithm : splitter;
+
+            enforce!HTTPAPIException(
+                this.json["encoding"].get!string()== "base64");
+
+            auto encoded = this.json["content"].get!string();
+            // GitHub provides base64 with newlines injected to enable per-line
+            // decoding, those have to be removed here
+            return Base64.decode(encoded.splitter("\n").join(""));
+        }
+    }
+
+    /**
+        Represents a directory stored in the repository
+     **/
+    static struct Directory
+    {
+        const Json json;
+
+        private this ( Json json )
+        {
+            this.json = json;
+        }
+
+        /**
+            Returns:
+                Array of paths for entities within this directory. Paths are
+                relative to repository root.
+         **/
+        const(string)[] listAll ( )
+        {
+            import std.algorithm.iteration : map;
+            import std.array;
+
+            return this.json
+                .get!(Json[])
+                .map!(element => element["path"].get!string())
+                .array();
+        }
+    }
+
+    /**
+        Represents a submodule linked from the repository
+     **/
+    static struct Submodule
+    {
+        const Json json;
+
+        private this ( Json json )
+        {
+            this.json = json;
+        }
+
+        /**
+            Returns:
+                linked submodule hash
+         **/
+        string sha ( )
+        {
+            return this.json["sha"].get!string();
+        }
+
+        /**
+            Returns:
+                linked submodule git URL
+         **/
+        string url ( )
+        {
+            return this.json["submodule_git_url"].get!string();
+        }
+    }
+
+    /**
+        Raw entity metadata JSON
+
+        See https://developer.github.com/v3/repos/contents/#get-contents for
+        more details
+     **/
+    const Json json;
+
+    /**
+        Returns:
+            typeid of whatever kind of entity stored metadata describes
+     **/
+    TypeInfo kind ( )
+    {
+        if (this.json.type() == Json.Type.Array)
+            return typeid(RepositoryEntity.Directory);
+
+        switch (this.json["type"].get!string())
+        {
+            case "file":
+                return typeid(RepositoryEntity.File);
+            case "submodule":
+                return typeid(RepositoryEntity.Submodule);
+            case "symlink":
+            default:
+                assert(false);
+        }
+    }
+
+    /**
+        Returns:
+            current entity wrapped as RepositoryEntity.File
+
+        Throws:
+            HTTPAPIException on expectation violation
+     **/
+    File expectFile ( )
+    {
+        import std.exception : enforce;
+        enforce!HTTPAPIException(this.kind == typeid(RepositoryEntity.File));
+        return File(json);
+    }
+
+    /**
+        Returns:
+            current entity wrapped as RepositoryEntity.Directory
+
+        Throws:
+            HTTPAPIException on expectation violation
+     **/
+    Directory expectDirectory ( )
+    {
+        import std.exception : enforce;
+        enforce!HTTPAPIException(this.kind == typeid(RepositoryEntity.Directory));
+        return Directory(json);
+    }
+
+    /**
+        Returns:
+            current entity wrapped as RepositoryEntity.Submodule
+
+        Throws:
+            HTTPAPIException on expectation violation
+     **/
+    Submodule expectSubmodule ( )
+    {
+        import std.exception : enforce;
+        enforce!HTTPAPIException(this.kind == typeid(RepositoryEntity.Submodule));
+        return Submodule(json);
+    }
+}
+
+/**
+    Fetch specific repository metadata
+
+    Params:
+        connection = setup connection to API server
+        repo = repository string of form "owner/repo", for example
+            "sociomantic-tsunami/ocean"
+
+    Returns:
+        Wrapper struct to work with that repo embedding the json metadata
+ **/
+Repository repository ( ref HTTPConnection connection, string repo )
+{
+    import std.format;
+
+    validateRepoString(repo);
+
+    return Repository(
+        &connection,
+        connection.get(format("/repos/%s", repo))
+    );
+}
+
+/**
+    Lists all repos for given organization
+
+    Params:
+        connection = setup connection to API server
+        name = organization name
+        type = project filter to use, as defined by
+            https://developer.github.com/v3/repos/#list-organization-repositories
+
+    Returns:
+        Array of json objects one per each repo
+ **/
+Repository[] listOrganizationRepos ( ref HTTPConnection connection, string name,
+    string type = "sources" )
+{
+    import std.format;
+    import std.exception : enforce;
+    import std.algorithm : map, canFind;
+    import std.array;
+
+    enforce!APIException(
+        canFind(["all", "public", "private", "forks", "sources", "member"], type),
+        "Unknown repository filter type"
+    );
+
+    auto url = format("/orgs/%s/repos?type=%s", name, type);
+    auto json = connection.get(url);
+
+    return json
+        .get!(Json[])
+        .map!(elem => Repository(&connection, elem))
+        .array();
+}
---
+++ src/octod/core.d
@@ -0,0 +1,420 @@
+/*******************************************************************************
+
+    Implements persistent HTTP connection to GitHub API server which provides
+    basic get/post/patch methods taking care of API details internally (like
+    auth or multi-page responses).
+
+    Copyright: Copyright (c) 2016 Sociomantic Labs GmbH. All rights reserved
+
+    License: Boost Software License Version 1.0 (see LICENSE for details)
+
+*******************************************************************************/
+
+module octod.core;
+
+import std.exception : enforce;
+
+import vibe.http.client;
+import vibe.data.json;
+import vibe.core.log;
+
+import octod.media;
+
+/**
+    Configuration required to interact with GitHub API
+ **/
+struct Configuration
+{
+    /// URL prepended to all API requests
+    string baseURL = "https://api.github.com";
+    /// If present, will be used as auth username
+    string username;
+    /// If 'this.username' is present, will be used as auth password
+    string password;
+    /// If 'this.username' is empty, will be used as auth token
+    string oauthToken;
+    /// By default client works in live mode
+    bool dryRun = false;
+}
+
+/**
+    Thrown upon any protocol/connection issues when trying to
+    interact with API HTTP server.
+ **/
+class HTTPAPIException : Exception
+{
+    this ( string msg, string file = __FILE__, ulong line = __LINE__ )
+    {
+        super(msg, file, line);
+    }
+}
+
+/**
+    Wrapper on top of vibe.d `connectHTTP`, describing persistent HTTP
+    connection to GitHub API server and providing convenience
+    `get`/`post`/`patch` methods taking care of GitHub HTTP specifics.
+
+    Does not implement any of higher level API interpretation, instead should
+    be used as a core facilities for such util.
+
+    Currently does not expose any of returned HTTP headers - it is not yet
+    clear if this will be necessary for implementing more complex API
+    methods.
+ **/
+struct HTTPConnection
+{
+    private
+    {
+        alias Connection = typeof(connectHTTP(null, 0, false));
+        Connection connection;
+
+        Configuration config;
+    }
+
+    /**
+        Setups new connection instance and attempts connecting to the
+        configured API server.
+
+        Params:
+            config = configuration used for interacting with API server
+
+        Returns:
+             instance of this struct connected to the API server and ready to
+             start sending requests
+     **/
+    static HTTPConnection connect ( Configuration config )
+    {
+        auto conn = typeof(this)(config);
+        conn.connect();
+        return conn;
+    }
+
+    /**
+        Constructor
+
+        `connect` method must be called on constructed instance before
+        it gets into usable state.
+
+        Params:
+            config = configuration used for interacting with API server
+     **/
+    this ( Configuration config )
+    {
+        import std.string : startsWith;
+
+        if (config.oauthToken.length > 0)
+        {
+            if (!config.oauthToken.startsWith("bearer "))
+                config.oauthToken = "bearer " ~ config.oauthToken;
+        }
+
+        this.config = config;
+    }
+
+    /**
+        Creates vibe.d persistent HTTP(S) connection to configured API
+        server.
+
+        Requires configured base URL to define explicit protocol (HTTP or HTTPS)
+     **/
+    void connect ( )
+    {
+        assert(this.connection is null);
+
+        import std.regex;
+
+        logTrace("Connecting to GitHub API server ...");
+
+        static rgxURL = regex(r"^(\w*)://([^/:]+)(:[^/]+)?$");
+        auto match = this.config.baseURL.matchFirst(rgxURL);
+
+        enforce!HTTPAPIException(
+            match.length >= 3,
+            "Malformed API base URL in configuration: " ~ this.config.baseURL
+        );
+
+        string addr = match[2];
+        ushort port;
+        bool   tls;
+
+        if (match.length == 4 && match[3].length > 0)
+        {
+            import std.conv : to;
+            port = to!ushort(match[3][1..$]);
+            tls  = (match[1] == "https");
+        }
+        else
+        {
+            switch (match[1])
+            {
+                case "http":
+                    port = 80;
+                    tls = false;
+                    break;
+                case "https":
+                    port = 443;
+                    tls = true;
+                    break;
+                default:
+                    throw new HTTPAPIException("Protocol not supported: " ~ match[1]);
+            }
+        }
+
+        this.connection = connectHTTP(addr, port, tls);
+
+        logTrace("Connected.");
+    }
+
+    /**
+        Sends GET request to API server
+
+        Params:
+            url = GitHub API method URL (relative)
+            accept = optional argument with custom `Accept` header to
+                specify for this one request
+
+        Returns:
+            Json body of the response. If response is multi-page, all pages
+            are collected and concatenated into one returned json object.
+     **/
+    Json get ( string url, MediaType accept )
+    {
+        assert (this.connection !is null);
+
+        logTrace("GET %s", url);
+
+        if (this.config.dryRun)
+            return Json.emptyObject;
+
+        // initialize result as array - if actual response isn't array, it
+        // will be overwritten by assignement anyway, otherwise it allows
+        // easy concatenation of multi-page results
+
+        Json result = Json.emptyArray;
+        HTTPClientResponse response;
+
+        url = this.config.baseURL ~ url;
+
+        while (true)
+        {
+            response = this.connection.request(
+                (scope request) {
+                    request.requestURL = url;
+                    request.method = HTTPMethod.GET;
+                    this.prepareRequest(request, accept.toString());
+                }
+            );
+
+            scope(exit)
+                response.dropBody();
+
+            if (auto location = this.handleResponseStatus(response))
+            {
+                url = location;
+                continue;
+            }
+
+            // Most responses are JSON, treat all others as plain text and
+            // wrap result into JSON string:
+            if (accept.format != MediaFormat.JSON)
+            {
+                import vibe.stream.operations;
+                return Json(response.bodyReader.readAllUTF8());
+            }
+
+            auto json = response.readJson();
+            if (json.type == Json.Type.Array)
+            {
+                foreach (element; json.get!(Json[]))
+                    result.appendArrayElement(element);
+            }
+            else
+                result = json;
+
+            // GitHub splits long response lists into several "pages", each
+            // needs to be retrieved by own request. If pages are present,
+            // they are defined in "Link" header:
+
+            import std.regex;
+
+            static rgxLink = regex(`<([^>]+)>;\s+rel="next"`);
+
+            if (auto link = "Link" in response.headers)
+            {
+                assert(result.type == Json.Type.Array);
+
+                auto match = (*link).matchFirst(rgxLink);
+                if (match.length == 2)
+                    url = match[1];
+                else
+                    break;
+            }
+            else
+                break;
+        }
+
+        return result;
+    }
+
+    /**
+        ditto
+     **/
+    Json get ( string url, string accept = "" )
+    {
+        return this.get(url, accept.length
+            ? MediaType.parse(accept) : MediaType.Default);
+    }
+
+    /**
+        Sends POST request to API server
+
+        Params:
+            url = GitHub API method URL (relative)
+            json = request body to send
+            accept = optional, request media type
+
+        Returns:
+            Json body of the response.
+     **/
+    Json post ( string url, Json json, MediaType accept )
+    {
+        assert (this.connection !is null);
+
+        logTrace("POST %s", url);
+
+        if (this.config.dryRun)
+            return Json.emptyObject;
+
+        auto response = this.connection.request(
+            (scope request) {
+                request.requestURL = url;
+                request.method = HTTPMethod.POST;
+                this.prepareRequest(request, accept.toString());
+                request.writeJsonBody(json);
+            }
+        );
+
+        scope(exit)
+            response.dropBody();
+
+        if (auto location = this.handleResponseStatus(response))
+        {
+            return this.post(location, json);
+        }
+
+        return response.readJson();
+    }
+
+    /**
+        ditto
+     **/
+    Json post ( string url, Json json, string accept = "")
+    {
+        return this.post(url, json, accept.length
+            ? MediaType.parse(accept) : MediaType.Default);
+    }
+
+    /**
+        Sends PATCH request to API server
+
+        Params:
+            url = GitHub API method URL (relative)
+            json = request body to send
+            accept = optional, request media type
+
+        Returns:
+            Json body of the response.
+     **/
+    Json patch ( string url, Json json, MediaType accept )
+    {
+        assert (this.connection !is null);
+
+        logTrace("PATCH %s", url);
+
+        if (this.config.dryRun)
+            return Json.emptyObject;
+
+        auto response = this.connection.request(
+            (scope request) {
+                request.requestURL = url;
+                request.method = HTTPMethod.PATCH;
+                this.prepareRequest(request, accept.toString());
+                request.writeJsonBody(json);
+            }
+        );
+
+        scope(exit)
+            response.dropBody();
+
+        if (auto location = this.handleResponseStatus(response))
+        {
+            return this.patch(location, json);
+        }
+
+        return response.readJson();
+    }
+
+    /**
+        ditto
+     **/
+    Json patch ( string url, Json json, string accept = "")
+    {
+        return this.patch(url, json, accept.length
+            ? MediaType.parse(accept) : MediaType.Default);
+    }
+
+    /**
+        Common request setup code shared by all request kinds
+
+        Params:
+            request = vibe.d request object to prepare
+            accept = optional argument with custom `Accept` header to
+                specify for this one request
+     **/
+    private void prepareRequest ( scope HTTPClientRequest request,
+        string accept = "" )
+    {
+        import vibe.http.auth.basic_auth : addBasicAuth;
+
+        if (this.config.username.length > 0)
+            request.addBasicAuth(this.config.username, this.config.password);
+        else if (this.config.oauthToken.length > 0)
+            request.headers["Authorization"] = this.config.oauthToken;
+
+        if (accept.length)
+            request.headers["Accept"] = accept;
+        else
+            request.headers["Accept"] = MediaType.Default.toString();
+    }
+
+    /**
+        Ensures that HTTP response succeeded
+
+        Params:
+            response = vibe.d response object that needs to be checked
+
+        Returns:
+            new URL string in case of redirect, null otherwise
+     **/
+    private string handleResponseStatus ( scope HTTPClientResponse response )
+    {
+        import std.format;
+        import vibe.http.status;
+
+        auto status = response.statusCode;
+
+        if (status == HTTPStatus.notFound)
+            throw new HTTPAPIException("Requested non-existent API URL");
+
+        if (status == HTTPStatus.found)
+            return response.headers["Location"];
+
+        enforce!HTTPAPIException(
+            status >= 200 && status < 300,
+            format("Expected status code 2xx, got %s\n\n%s\n",
+                response.statusCode, response.readJson())
+        );
+
+        return null;
+    }
+}

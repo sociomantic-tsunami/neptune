328f9811280127af75e3508cc4f9b5e4eaef302a
Jens Mueller <jens.mueller@dunnhumby.com>
2019-06-06 11:38:32 +0200

    Add skeleton for time_iopen application
    This adds the skeleton to build the empty application called time_open.
    The purpose of the application is to list latest open pull requests for
    a repository.

---
+++ src/time_open/main.d
@@ -0,0 +1,29 @@
+/*******************************************************************************
+
+    Main file
+
+    Copyright:
+        Copyright (c) 2019 dunnhumby Germany GmbH. All rights reserved.
+
+    License:
+        Boost Software License Version 1.0. See LICENSE.txt for details.
+
+*******************************************************************************/
+
+module time_open.main;
+
+
+/*******************************************************************************
+
+    Main function
+
+    Params:
+        args = command line arguments
+
+*******************************************************************************/
+
+version(unittest) {} else
+int main ( string[] args )
+{
+    return 0;
+}
